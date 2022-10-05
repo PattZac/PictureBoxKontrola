f5bca690aaa8af2496f95d5e35ae04dcf25087af
Pat <113007904+PattZac@users.noreply.github.com>
2022-10-05 09:00:13 +0200

    Add files via upload

---
+++ PictureBoxKontrola/PictureBoxKontrola/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Visible == false) {
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+            }
+            
+        }
+    }
+}
